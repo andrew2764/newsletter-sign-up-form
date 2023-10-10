fef5ae63a827fe986833d070d30db3ea4254f9b6
andrew <andrew2764@gmail.com>
2023-10-10 20:39:07 +0800

    Dynamically redirect to success display

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("form");
+const mainElem = document.querySelector("main");
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -11,10 +12,28 @@ form.addEventListener("submit", (e) => {
       "afterend",
       `<p class="error">Please enter your email address</p>`
     );
-  } else if (!emailRegex.test(emailInput.value)) {
+    return;
+  }
+  if (!emailRegex.test(emailInput.value)) {
     emailInput.insertAdjacentHTML(
       "afterend",
       `<p class="error">Please enter a valid email address</p>`
     );
+    return;
   }
+  mainElem.style.maxWidth = "500px";
+  mainElem.innerHTML = /*html*/ `
+    <content class="success--container">
+      <div class="content__success">
+        <div class="success__image--container"><img src="./assets/images/icon-success.svg" alt="success icon"
+            class="img__success"></div>
+        <h1>Thanks for subscribing!</h1>
+        <p>
+          A confirmation email has been sent to <strong>ash@loremcompany.com</strong>.
+          Please open it and click the button inside to confirm your subscription.
+        </p>
+      </div>
+      <form action="/"><button>Dismiss message</button></form>
+    </content>
+  `;
 });
